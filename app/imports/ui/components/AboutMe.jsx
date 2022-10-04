b5486777e0aaa5a9bf326649aeca1dd713243410
marcos buccat <buccatm@hawaii.edu>
2022-10-03 17:54:00 -1000

    Update part 3
    not finished

---
+++ app/imports/ui/components/AboutMe.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Container, Col, Card, Row, Image, ListGroup } from 'react-bootstrap';
+
+/** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
+const Footer = () => (
+  <Container fluid>
+    <Card>
+      <Card.Title>
+        <h1>
+          About Presenter
+        </h1>
+      </Card.Title>
+      <Card.Body>
+        <Row style={{ marginTop: '4rem' }} className="justify-content-center" xxl="auto" xl="auto" lg="auto" md="auto">
+          <Col>
+            <Image src="/images/meteor-logo.png" roundedCircle />
+          </Col>
+          <Col xxl="2" xl="2" lg="2" md="2" />
+          <Col>
+            <h2>
+              Marcos Buccat Jr.
+            </h2>
+            <h4>
+              <p>
+                <a href="https://buccatm.github.io/" target="_blank" rel="noreferrer">GitHub Page</a>
+              </p>
+            </h4>
+            <br />
+            <h3>
+              Framework & Libraries
+            </h3>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <a href="https://guide.meteor.com/react.html" target="_blank" rel="noreferrer">
+                  Meteor React
+                </a>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <a href="https://react-bootstrap.github.io/getting-started/introduction" target="_blank" rel="noreferrer">
+                  React Bootstrap
+                </a>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <a href="https://plotly.com/javascript/react/" target="_blank" rel="noreferrer">
+                  Plotly.js React
+                </a>
+              </ListGroup.Item>
+            </ListGroup>
+            <br />
+            <h3>
+              Datas
+            </h3>
+            <ListGroup variant="flush">
+              <ListGroup.Item>Data 1</ListGroup.Item>
+              <ListGroup.Item>Data 2</ListGroup.Item>
+              <ListGroup.Item>Data 3</ListGroup.Item>
+            </ListGroup>
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
+    <br />
+  </Container>
+);
+
+export default Footer;
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -3,7 +3,9 @@ import { Card, Col, Container, Image, ListGroup, Row, Tab, Tabs } from 'react-bo
 import { PAGE_IDS } from '../utilities/PageIDs';
 import Part1Stuff from '../components/Part1Stuff';
 import Part2Stuff from '../components/Part2Stuff';
-import UKcensus1851Set from "../components/UKcensus1851Set";
+import UKcensus1851Set from '../components/UKcensus1851Set';
+import Part3Stuff from '../components/Part3Stuff';
+import AboutMe from "../components/AboutMe";
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => {
@@ -38,67 +40,12 @@ const Landing = () => {
             <br />
             <UKcensus1851Set />
           </Tab>
-          <Tab eventKey="part 3" title="Contact" />
+          <Tab eventKey="part 3" title="Part 3">
+            <Part3Stuff />
+          </Tab>
         </Tabs>
       </Container>
-      <Container fluid>
-        <Card>
-          <Card.Title>
-            <h1>
-              About Presenter
-            </h1>
-          </Card.Title>
-          <Card.Body>
-            <Row style={{ marginTop: '4rem' }} className="justify-content-center" xxl="auto" xl="auto" lg="auto" md="auto">
-              <Col>
-                <Image src="/images/meteor-logo.png" roundedCircle />
-              </Col>
-              <Col xxl="2" xl="2" lg="2" md="2" />
-              <Col>
-                <h2>
-                  Marcos Buccat Jr.
-                </h2>
-                <h4>
-                  <p>
-                    <a href="https://buccatm.github.io/" target="_blank" rel="noreferrer">GitHub Page</a>
-                  </p>
-                </h4>
-                <br />
-                <h3>
-                  Framework & Libraries
-                </h3>
-                <ListGroup variant="flush">
-                  <ListGroup.Item>
-                    <a href="https://guide.meteor.com/react.html" target="_blank" rel="noreferrer">
-                      Meteor React
-                    </a>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <a href="https://react-bootstrap.github.io/getting-started/introduction" target="_blank" rel="noreferrer">
-                      React Bootstrap
-                    </a>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <a href="https://plotly.com/javascript/react/" target="_blank" rel="noreferrer">
-                      Plotly.js React
-                    </a>
-                  </ListGroup.Item>
-                </ListGroup>
-                <br />
-                <h3>
-                  Datas
-                </h3>
-                <ListGroup variant="flush">
-                  <ListGroup.Item>Data 1</ListGroup.Item>
-                  <ListGroup.Item>Data 2</ListGroup.Item>
-                  <ListGroup.Item>Data 3</ListGroup.Item>
-                </ListGroup>
-              </Col>
-            </Row>
-          </Card.Body>
-        </Card>
-        <br />
-      </Container>
+      <AboutMe />
     </Container>
   );
 };
